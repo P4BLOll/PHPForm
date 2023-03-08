d3cfd09f49aed49c4327718aeb635f05b517dfeb
Pablo Santos <99838818+P4BLOll@users.noreply.github.com>
2023-03-08 00:54:37 -0300

    Add files via upload

--- FormulárioPHP/index.php
+++ FormulárioPHP/index.php
@@ -7,7 +7,7 @@
     <body>
         <div class="container">
             <form>
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <nav class="navbar navbar-expand-lg bg-primary">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="#">SISTEMA WEB</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,10 +16,10 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
+                                <a class="nav-link" href="#">Consultar</a>
                             </li>
                         </ul>
                     </div>
@@ -28,21 +28,38 @@
                 <div class="inputs">
                     <h2>Cadastrar - Agendamento de Potenciais Clientes</h2>
                     <p>Sistema Utilizado para Agendamento de Serviços</p>
+
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        <label for="formGroupExampleInput" class="form-label">Nome:</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput">
                     </div>
+
+                    <div class="form-outline">
+                        <label class="form-label" for="typePhone">Telefone:</label>
+                        <input type="tel" id="typePhone" class="form-control" placeholder="(xx)xxxxx-xxxx">
+                    </div>
+
+                    <div class="col-auto">
+                        <label for="formGroupExampleInput" class="form-label">Origem</label>
+                        <select class="form-select" id="autoSizingSelect">
+                            <option value="1">Celular</option>
+                            <option value="2">Outro...</option>
+                        </select>
+                    </div>
+
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label for="formGroupExampleInput" class="form-label">Data do Contato</label>
+                        <input type="date" class="form-control" id="formGroupExampleInput" value="dd/mm/yy" min="200-01-01" max="2025-12-31">
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Observação:</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
+
+                    <button type="submit" class="btn btn-primary">Cadastrar</button>
+
+                </div>
             </form>
         </div>
     </body>
